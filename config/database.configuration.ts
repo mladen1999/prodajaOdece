ae8afee43f89ac7c36f1f808e576de2636e82000
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-01 17:24:02 +0100

    Create the administrator table service

---
+++ config/database.configuration.ts
@@ -0,0 +1,8 @@
+import { hostname, userInfo } from "os";
+
+export const DatabaseConfiguration = {
+    hostname: 'localhost',
+    username: 'mladen',
+    password: 'mladen1999',
+    database: 'odeca'
+};
