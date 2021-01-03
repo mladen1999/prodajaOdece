937292c21c9fde346631fe997267cd835f0fab91
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-03 18:03:14 +0100

    Implementacija globalnog API response mehanizma

--- src/controllers/api/administrator.controller.ts
+++ src/controllers/api/administrator.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { Administrator } from "entities/administrator.entity";
 import { AddAdministratorDto } from "src/dtos/administrator/add.administrator.dto";
 import { EditAdministratorDto } from "src/dtos/administrator/edit.administrator.dto";
+import { ApiResponse } from "src/misc/api.response.class";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 
 @Controller('api/administrator')
@@ -16,14 +17,23 @@ export class AdministratorController {
     }
 
     @Get(':id') // GET: http://localhost:3000/api/administrator/4/
-    getById( @Param('id') administratorId: number): Promise<Administrator> {
-        return this.administratorService.getById(administratorId);
+    getById( @Param('id') administratorId: number): Promise<Administrator | ApiResponse> {
+        return new Promise(async (resolve) => {
+            let admin = await this.administratorService.getById(administratorId);
+
+            if(admin === undefined) {
+                resolve(new ApiResponse("error", -1002));
+            }
+
+            resolve(admin);
+        });
+        
     }
 
     // Dodavanje novog administratora
     // PUT Metod: Kada radimo dodavanje novih zapisa koristimo @Put metod
     @Put() // PUT: http://localhost:3000/api/administrator/
-    add(@Body() data: AddAdministratorDto): Promise<Administrator> {
+    add(@Body() data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
         return this.administratorService.add(data); // data je DTO
     }
 
@@ -31,7 +41,7 @@ export class AdministratorController {
     // POST Metod obecava da cemo moci da editujemo informacije o administratoru ALI SAMO PASSWORD SE MENJA
     // POST Metod sluzi za editovanje
     @Post(':id') // GET: http://localhost:3000/api/administrator/4/
-    edit(@Param('id') id: number, @Body() data: EditAdministratorDto): Promise<Administrator> {
+    edit(@Param('id') id: number, @Body() data: EditAdministratorDto): Promise<Administrator | ApiResponse> {
         return this.administratorService.editById(id, data);
     }
 }
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -5,6 +5,7 @@ import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dt
 import { Repository } from 'typeorm';
 import * as crypto from 'crypto'
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 
 @Injectable()
 export class AdministratorService {
@@ -22,7 +23,7 @@ export class AdministratorService {
     }
 
     // add Funkcija
-    add(data: AddAdministratorDto): Promise<Administrator> {
+    add(data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
         // DTO => Nas model
         // username => username
         // password =[~]==> passwordHash ! Stvar izbora!   |   Mi cemo koristiti SHA512 za passwordHash
@@ -35,11 +36,24 @@ export class AdministratorService {
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHashString;
 
-        return this.administrator.save(newAdmin);
+        return new Promise((resolve) => {
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            });
+        });
     }
 
-    async editById(id: number, data: EditAdministratorDto): Promise<Administrator> {
+    async editById(id: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse> {
         let admin: Administrator = await this.administrator.findOne(id);
+
+        if(admin === undefined) {
+            return new Promise((resolve) => {
+                resolve(new ApiResponse("error", -1002));
+            });
+        }
 
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
