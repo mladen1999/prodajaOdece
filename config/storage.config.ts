82d33b058bfd94eb5067cd74c2ad9bae38a19e19
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-28 09:28:48 +0100

    Implemen... meh... RealMimeType i velicine slike

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,4 +1,6 @@
 export const StorageConfig = {
     photoDestination: '../storage/photos/',
-    photoMaxFileSize: 1024 * 1024 * 3 // u bitovima koliko prohvatamo ( u nasem slucaju 3 MB )
+    photoMaxFileSize: 1024 * 1024 * 3, // u bitovima koliko prohvatamo ( u nasem slucaju 3 MB )
+    photoThumbSize: { width: 120, height: 100 },
+    photoSmallSize: { width: 320, height: 240 }
 };
--- src/controllers/api/article.controller.ts
+++ src/controllers/api/article.controller.ts
@@ -9,6 +9,9 @@ import { StorageConfig } from "config/storage.config";
 import { PhotoService } from "src/services/photo/photo.service";
 import { Photo } from "entities/photo.entity";
 import { ApiResponse } from "src/misc/api.response.class";
+import * as fileType from 'file-type';
+import * as fs from 'fs';
+import * as sharp from 'sharp';
 
 @Controller('api/article')
 @Crud({ // Crud kontroler koristi vec predefinisane setove operacija tj predefinisane funkcije koje rade CRUD na osnovu CRUD akotacija
@@ -123,9 +126,26 @@ export class ArticleController {
             return new ApiResponse('error', -4002, 'File not uploaded!'); // Slika ne postoji
         }
 
+        const fileTypeResult = await fileType.fromFile(photo.path);
+        if(!fileTypeResult) {
+            // TODO: Obrisati taj fajl
+            fs.unlinkSync(photo.path);
+            //
+            return new ApiResponse('error', -4002, 'Cannot detect file type!'); // File ne valja
+        }
+
         // TODO: Real Mime Type check
+        const realMimeType = fileTypeResult.mime;
+        if(!(realMimeType.includes('jpeg') || realMimeType.includes('png'))) {
+            // TODO: Obrisati taj fajl
+            fs.unlinkSync(photo.path);
+            //
+            return new ApiResponse('error', -4002, 'Bad file content type!'); // File ne valja
+        }
 
         // TODO: Save a resized file
+        await this.createThumb(photo);
+        await this.createSmallImage(photo);
 
         const newPhoto: Photo = new Photo();
         newPhoto.articleId = articleId;
@@ -137,5 +157,42 @@ export class ArticleController {
         }
 
         return savedPhoto;
+    }
+
+    async createThumb(photo) {
+        const originalFilePath = photo.path;
+        const fileName = photo.filename;
+
+        const destinationFilePath = StorageConfig.photoDestination + "thumb/" + fileName;
+
+        await sharp(originalFilePath)
+        .resize({
+            fit: 'cover',
+            width: StorageConfig.photoThumbSize.width,
+            height: StorageConfig.photoThumbSize.height,
+            background: {
+                r: 255, g: 255, b: 255, alpha: 0.0
+            }
+
+        })
+        .toFile(destinationFilePath);
+    }
+
+    async createSmallImage(photo) {
+        const originalFilePath = photo.path;
+        const fileName = photo.filename;
+
+        const destinationFilePath = StorageConfig.photoDestination + "small/" + fileName;
+
+        await sharp(originalFilePath)
+        .resize({
+            fit: 'cover',
+            width: StorageConfig.photoSmallSize.width,
+            height: StorageConfig.photoSmallSize.height,
+            background: {
+                r: 255, g: 255, b: 255, alpha: 0.0
+            }
+        })
+        .toFile(destinationFilePath);
     }
 }
