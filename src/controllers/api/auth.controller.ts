01e3c13eed52b764a2361d49f74ab2b21b5fb61d
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-29 13:34:47 +0100

    Implementacija registracije korisnika

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Post, Put, Req } from "@nestjs/common";
 import { Administrator } from "src/entities/administrator.entity";
 import { LoginAdministratorDto } from "src/dtos/administrator/login.administrator.dto";
 import { ApiResponse } from "src/misc/api.response.class";
@@ -10,10 +10,15 @@ import * as jwt from 'jsonwebtoken';
 import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator.dto";
 import { Request } from "express";
 import { jwtSecret } from "config/jwt.secret";
+import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
+import { UserService } from "src/services/user/user.service";
 
 @Controller('auth')
 export class AuthController {
-    constructor(public administratorService: AdministratorService) { }
+    constructor(
+        public administratorService: AdministratorService,
+        public userService: UserService,
+        ) { }
 
     // Proces logovanja korisnika
     @Post('login') // http://localhost:3000/auth/login/
@@ -64,5 +69,10 @@ export class AuthController {
 
         return new Promise(resolve => resolve(responseObject));
 
+    }
+
+    @Put('user/register') // PUT http://localhost:3000/auth/user/register/
+    async userRegister(@Body() data: UserRegistrationDto) {
+        return await this.userService.register(data);
     }
 }
