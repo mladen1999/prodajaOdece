29c362f17d7e256e84632dc94bfb22d5f330afa3
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-29 14:35:58 +0100

    Odvajanje login-a za admina i usera

--- src/controllers/api/auth.controller.ts
+++ src/controllers/api/auth.controller.ts
@@ -5,13 +5,14 @@ import { ApiResponse } from "src/misc/api.response.class";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 import * as crypto from 'crypto'
 import { resolve } from "path";
-import { LoginInfoAdministratorDto } from "src/dtos/administrator/login.info.administrator.dto";
+import { LoginInfoDto } from "src/dtos/auth/login.info.dto";
 import * as jwt from 'jsonwebtoken';
-import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator.dto";
+import { JwtDataDto } from "src/dtos/auth/jwt.data.dto";
 import { Request } from "express";
 import { jwtSecret } from "config/jwt.secret";
 import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { UserService } from "src/services/user/user.service";
+import { LoginUserDto } from "src/dtos/user/login.user.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -21,8 +22,8 @@ export class AuthController {
         ) { }
 
     // Proces logovanja korisnika
-    @Post('login') // http://localhost:3000/auth/login/
-    async doLogin(@Body() data: LoginAdministratorDto, @Req() req: Request): Promise<LoginInfoAdministratorDto | ApiResponse> {
+    @Post('administrator/login') // http://localhost:3000/auth/administrator/login/
+    async doAdministratorLogin(@Body() data: LoginAdministratorDto, @Req() req: Request): Promise<LoginInfoDto | ApiResponse> {
         const administrator = await this.administratorService.getByUsername(data.username);
 
         if(!administrator) {
@@ -47,9 +48,10 @@ export class AuthController {
         // Sifrovanje ( TAJNA SIFRA => JSON ) => Sifrat binarni => HEX
         // HEXSTRING
 
-        const jwtData = new JwtDataAdministratorDto();
-        jwtData.administratorId = administrator.administratorId;
-        jwtData.username = administrator.username;
+        const jwtData = new JwtDataDto();
+        jwtData.role = "administrator";
+        jwtData.id = administrator.administratorId;
+        jwtData.identity = administrator.username;
 
         let sada = new Date();
         sada.setDate(sada.getDate() + 14); // 14 dana traje nas 'sada'
@@ -61,7 +63,7 @@ export class AuthController {
 
         let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret); // Ovde se generise token
 
-        const responseObject = new LoginInfoAdministratorDto(
+        const responseObject = new LoginInfoDto(
             administrator.administratorId,
             administrator.username,
             token
@@ -74,5 +76,56 @@ export class AuthController {
     @Put('user/register') // PUT http://localhost:3000/auth/user/register/
     async userRegister(@Body() data: UserRegistrationDto) {
         return await this.userService.register(data);
+    }
+
+    @Post('user/login') // http://localhost:3000/auth/user/login/
+    async doUserLogin(@Body() data: LoginUserDto, @Req() req: Request): Promise<LoginInfoDto | ApiResponse> {
+        const user = await this.userService.getByEmail(data.email);
+
+        if(!user) {
+            // Admin nije pronadjen
+            return new Promise(resolve => resolve(new ApiResponse('error', -3001)));
+        }
+
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        if(user.passwordHash !== passwordHashString) {
+            // Password nije ispravan
+            return new Promise(resolve => resolve(new ApiResponse('error', -3002)));
+        }
+
+        // administratorId
+        // username
+        // token (JWT) => JSON Web Tokeni je sifrovan odredjenim tajnim kodom
+        // Tajna sifra
+        // JSON = { administratorId, username, exp, ip, userAgent }
+        // Sifrovanje ( TAJNA SIFRA => JSON ) => Sifrat binarni => HEX
+        // HEXSTRING
+
+        const jwtData = new JwtDataDto();
+        jwtData.role = "user";
+        jwtData.id = user.userId;
+        jwtData.identity = user.email;
+
+        let sada = new Date();
+        sada.setDate(sada.getDate() + 14); // 14 dana traje nas 'sada'
+        const istekTimestamp = sada.getTime() / 1000;
+        jwtData.exp = istekTimestamp;
+
+        jwtData.ip = req.ip.toString();
+        jwtData.ua = req.headers["user-agent"];
+
+        let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret); // Ovde se generise token
+
+        const responseObject = new LoginInfoDto(
+            user.userId,
+            user.email,
+            token
+        );
+
+        return new Promise(resolve => resolve(responseObject));
+
     }
 }
