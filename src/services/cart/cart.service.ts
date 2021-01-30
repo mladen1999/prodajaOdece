664ef804c1aadc50b96b73fc66fd6145bd73f560
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-30 17:30:03 +0100

    Implementacija korpe

---
+++ src/services/cart/cart.service.ts
@@ -0,0 +1,111 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Article } from "src/entities/article.entity";
+import { CartArticle } from "src/entities/cart-article.entity";
+import { Cart } from "src/entities/cart.entity";
+import { Order } from "src/entities/order.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class CartService {
+    constructor(
+        @InjectRepository(Cart)
+        private readonly cart: Repository<Cart>,
+
+        @InjectRepository(CartArticle)
+        private readonly cartArticle: Repository<CartArticle>,
+
+        @InjectRepository(Article)
+        private readonly article: Repository<Article>,
+
+        @InjectRepository(Order)
+        private readonly order: Repository<Order>,
+    ) { }
+
+    async getLastActiveCartByUserId(userId: number): Promise<Cart | null> {
+        const carts = await this.cart.find({
+            where: {
+                userId: userId,
+            },
+            order: {
+                createdAt: "DESC",
+            },
+            take: 1,
+            relations: [ "order" ],
+        });
+
+        // Ako nema korpi
+        if(!carts || carts.length === 0) {
+            return null;
+        }
+
+        const cart = carts[0];
+        if(cart.order !== null) {
+            return null;
+        }
+
+        return cart;
+
+    }
+
+    // Kreiranje korpe, popunjavanje i cuvanje
+    async createNewCartForUser(userId: number): Promise<Cart> {
+        const newCart: Cart = new Cart();
+        newCart.userId = userId;
+        return await this.cart.save(newCart);
+    }
+
+    // Dodavanje robe u korpi
+    async addArticleToCart(cartId: number, articleId: number, quantity: number): Promise<Cart> {
+        let record: CartArticle = await this.cartArticle.findOne({
+            cartId: cartId,
+            articleId: articleId,
+        });
+
+        if(!record) {
+            record = new CartArticle();
+            record.cartId = cartId;
+            record.articleId = articleId;
+            record.quantity = quantity;
+        } else {
+            record.quantity += quantity;
+        }
+
+        await this.cartArticle.save(record);
+        // return this.cartArticle.save(record); // PROMENI
+
+        return this.getById(cartId);
+    }
+
+    // Informacije o kopri
+    async getById(cartId: number): Promise<Cart> {
+        return await this.cart.findOne(cartId, {
+            relations: [
+                "user",
+                "cartArticles",
+                "cartArticles.article",
+                "cartArticles.article.category",      
+            ],
+        });
+    }
+
+    async changeQuantity(cartId: number, articleId: number, newQuantity: number): Promise<Cart> {
+        let record: CartArticle = await this.cartArticle.findOne({
+            cartId: cartId,
+            articleId: articleId,
+        });
+
+        //if(!record) {
+        if(record) {
+            record.quantity = newQuantity;
+
+            if(record.quantity === 0) {
+                await this.cartArticle.delete(record.cartArticleId);
+            } else {
+                await this.cartArticle.save(record);
+            }
+        }
+
+        return await this.getById(cartId);
+    }
+}
