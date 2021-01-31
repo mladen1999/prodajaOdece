f727f5647c78db7b8100f5d90dc8199dbbad84b3
Mladen Cvetkovic <mladenvts@gmail.com>
2021-01-31 09:38:25 +0100

    Dodavanje mehanizma za validaciju podataka

--- src/dtos/user/login.user.dto.ts
+++ src/dtos/user/login.user.dto.ts
@@ -1,4 +1,17 @@
+import * as Validator from 'class-validator';
+
 export class LoginUserDto{
+
+    @Validator.IsNotEmpty()
+    @Validator.IsEmail({
+        allow_ip_domain: false, // Ne moze biti email => mladen@127.0.0.1
+        allow_utf8_local_part: true,
+        require_tld: true, // Mora biti mladenvts@gmail.com ( MORA IMATI TLD => Top Level Domen)
+    })
     email: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(6, 128)
     password: string;
 }
--- src/dtos/user/user.registration.dto.ts
+++ src/dtos/user/user.registration.dto.ts
@@ -1,8 +1,36 @@
+import * as Validator from 'class-validator';
+
 export class UserRegistrationDto {
+
+    @Validator.IsNotEmpty()
+    @Validator.IsEmail({
+        allow_ip_domain: false, // Ne moze biti email => mladen@127.0.0.1
+        allow_utf8_local_part: true,
+        require_tld: true, // Mora biti mladenvts@gmail.com ( MORA IMATI TLD => Top Level Domen)
+    })
     email: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(6, 128)
     password: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(2, 64)
     forename: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(2, 64)
     surname: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsPhoneNumber(null) // Korisnik mora da unese +381 ...
     phoneNumber: string;
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(10, 512)
     postalAddress: string;
 }
